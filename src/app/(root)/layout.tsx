1191f4484309f511cc94afb974552981d476bfd8
Jaspreet Singh <44422128+ijaspreetsinghh@users.noreply.github.com>
2024-02-19 20:30:21 +0530

    added image uploader added cloudinary

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import MobileNav from "@/components/shared/MobileNav";
 import Sidebar from "@/components/shared/Sidebar";
+import { Toaster } from "@/components/ui/toaster";
 import React, { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
@@ -10,6 +11,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
       <div className='root-container'>
         <div className='wrapper'>{children}</div>
       </div>
+      <Toaster />
     </main>
   );
 };
---
+++ src/components/shared/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface Props {
+  title: string;
+  subtitle?: string;
+}
+const Header = ({ title, subtitle }: Props) => {
+  return (
+    <>
+      <h2 className='h2-bold text-dark-500'>{title}</h2>
+      {subtitle && <p className='p-16-regular mt-4'>{subtitle}</p>}
+    </>
+  );
+};
+
+export default Header;
